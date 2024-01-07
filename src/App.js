334c672b939673378b049105cca8a471ba0bf784
Promise Stephen <promisestephen29@gmail.com>
2024-01-07 10:04:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import {useState} from 'react';
-import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-import CityWeather from './CityWeather'
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import CityWeather from "./CityWeather";
+import "./App.css";
 
 function App() {
-
-  const [input, setInput] = useState("")
-  const [message, setMessage] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-  const [cityWeathers, setCityWeathers] = useState([])
+  const [input, setInput] = useState("");
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [cityWeathers, setCityWeathers] = useState([]);
 
   const apiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 
   function handleWeatherCheck(e) {
     e.preventDefault();
-    setIsLoading(true)
-
+    setIsLoading(true);
 
     if (cityWeathers.length > 0) {
       let inputValue = input;
@@ -38,71 +36,76 @@ function App() {
         }
         return content.toLowerCase() === inputValue.toLowerCase();
       });
-  
+
       if (filteredArray.length > 0) {
-        setMessage(`You already know the weather for ${inputValue} ...otherwise be more specific by providing the country code as well 😉`)
-        setInput("")
-        setIsLoading(false)
+        setMessage(
+          `You already know the weather for ${inputValue} ...otherwise be more specific by providing the country code as well 😉`
+        );
+        setInput("");
+        setIsLoading(false);
         return;
       }
     }
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric`;
-    axios.get(url)
-    .then((response) => {
-      console.log(response)
-      const { main, name, sys, weather } = response.data;
-      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
-      const cityWeather = {
-        main,
-        name,
-        sys,
-        weather,
-        icon
-      }
-      setCityWeathers([...cityWeathers, cityWeather])
-    })
-    .catch((err) => {
-      console.log(err)
-      setMessage("Sorry, we can't find the city entered");
-    });
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response);
+        const { main, name, sys, weather } = response.data;
+        const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+        const cityWeather = {
+          main,
+          name,
+          sys,
+          weather,
+          icon,
+        };
+        setCityWeathers([...cityWeathers, cityWeather]);
+      })
+      .catch(() => {
+        setMessage("Sorry, we can't find the city entered");
+      });
 
     setMessage("");
-    setInput("")
-    setIsLoading(false)
+    setInput("");
+    setIsLoading(false);
   }
   return (
     <>
-    <section className="top-banner">
-      <div className="container">
-        <h1 className="heading">Javascript Weather App</h1>
-        <form onSubmit={handleWeatherCheck}>
-          <input type="text" placeholder="Search for a city" 
-            value={input} onChange={(e) => setInput(e.target.value)} autoFocus 
-          />
-          <button type="submit">SUBMIT</button>
-          <span className="msg">{message}</span>
-        </form>
-      </div>
-    </section>
-    <section className="ajax-section">
-      <div className="container">
-        <ul className="cities">
-        {
-          (cityWeathers && cityWeathers.length > 0) && (
-            cityWeathers.map(cityWeather => {
-              return (
-              <CityWeather cityWeather = {cityWeather} key={`${cityWeather.name} ${cityWeather.sys.country}`} />
-            )
-            })
-          )
-        }
-        {
-          isLoading && <FontAwesomeIcon icon={faSpinner} color="white" />
-        }
-        </ul>
-      </div>
-    </section>
+      <section className="top-banner">
+        <div className="container">
+          <h1 className="heading">Javascript Weather App</h1>
+          <form onSubmit={handleWeatherCheck}>
+            <input
+              type="text"
+              placeholder="Search for a city"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              autoFocus
+            />
+            <button type="submit">SUBMIT</button>
+            <span className="msg">{message}</span>
+          </form>
+        </div>
+      </section>
+      <section className="ajax-section">
+        <div className="container">
+          <ul className="cities">
+            {cityWeathers &&
+              cityWeathers.length > 0 &&
+              cityWeathers.map((cityWeather) => {
+                return (
+                  <CityWeather
+                    cityWeather={cityWeather}
+                    key={`${cityWeather.name} ${cityWeather.sys.country}`}
+                  />
+                );
+              })}
+            {isLoading && <FontAwesomeIcon icon={faSpinner} color="white" />}
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
